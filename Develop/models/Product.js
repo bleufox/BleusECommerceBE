361a7b3b06b72c80f41e7df19e3e7244d76f75ef
bleufox <thebleufox8@gmail.com>
2021-09-21 13:08:58 -0700

    Added validate requirements to Product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -22,14 +22,21 @@ Product.init(
     prices: {
       type: DataTypes.DECIMAL(18,2),
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'category',
         key: 'id',
