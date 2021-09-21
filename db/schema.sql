ca1a8a97e3b4fa6a8dcc9286fa5a7200d9aebf8e
bleufox <thebleufox8@gmail.com>
2021-09-21 11:32:02 -0700

    Added model requirements to Categories

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-
-USE ecommerce_db;
-
-CREATE TABLE category (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    category_name IS NOT NULL;
-);
-
-CREATE TABLE product (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_name IS NOT NULL;
-    price DEC NOT NULL
-);
-
-CREATE TABLE category (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    category_name IS NOT NULL;
-);
-
-CREATE TABLE category (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    category_name IS NOT NULL;
-);
